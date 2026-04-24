88f51fd073affe01455e01d74a5b7e5443e35a0e
samuelnehrer02 <nehrer.samuel@gmail.com>
2026-04-24 10:23:49 +0200

    Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using JuliaFormatter: JuliaFormatter
 
 @testset verbose = true "ActiveInference tests" begin
     @testset "Code formatting" begin
-        @test JuliaFormatter.format(ActiveInference; verbose = false, overwrite = false)
+        @test JuliaFormatter.format(ActiveInference; verbose = true, overwrite = false)
     end
 
     @testset "Code linting" begin
