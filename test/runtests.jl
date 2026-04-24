51f224c51f19211d48e274498c0b4d86a124cea8
samuelnehrer02 <nehrer.samuel@gmail.com>
2026-04-24 10:52:47 +0200

    Removed JuliaFormatter

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,8 @@ using Test
 using Aqua
 using JET
 using ActiveInference
-using JuliaFormatter: JuliaFormatter
 
 @testset verbose = true "ActiveInference tests" begin
-    @testset "Code formatting" begin
-        @test JuliaFormatter.format(ActiveInference; verbose = true, overwrite = false)
-    end
-
     @testset "Code linting" begin
         JET.test_package(ActiveInference; target_modules = (ActiveInference,))
     end
